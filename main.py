f019666365c58975723424e7d9e9ff9039cbf0d4
aditiaputra100 <aditia_putra100@gmail.com>
2025-05-18 21:53:40 +0700

    add highscore feature and save to json file

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from enum import Enum
 import random
+import json
+import os
 
+FILE_NAME = 'highscores.json'
 
 class Difficulty(Enum):
     EASY = 10
     MEDIUM = 5
     HARD = 3
 
+def load_highscores():
+    if not os.path.exists(FILE_NAME):
+        return {d.name: None for d in Difficulty}
+
+    with open(FILE_NAME, 'r') as file_json:
+        return json.load(file_json)
+
+def save_highscores(highscores):
+    with open(FILE_NAME, 'w') as file_json:
+        json.dump(highscores, file_json, indent=4)
+
 def main():
+    highscores: dict | None = load_highscores()
+
     welcome_script: str = '''Welcome to the Number Guessing Game!
     \rI'm thinking of a number between 1 and 100.
     \rYou have 5 chances to guess the correct number.
@@ -25,7 +41,11 @@ def main():
     print('Please select the difficulty level:')
 
     for index, (difficulty, chances) in enumerate(difficulty_chances.items()):
-        print(f'{index + 1}. {difficulty.capitalize()} ({chances} chances)')
+
+        best_score = highscores.get(difficulty.upper())
+        best_score_str = f' | Best: {best_score if best_score else 0} tries'
+
+        print(f'{index + 1}. {difficulty.capitalize()} ({chances} chances)', best_score_str if highscores is not None else '')
 
     # Input choice
     input_choice: int = int(input("Enter your choice: " ))
@@ -41,6 +61,8 @@ def main():
     user_chances: int = difficulty_choice.value
     is_correct: bool = False
 
+    life = user_chances + 1
+
     while 0 < user_chances:
         # Input user guess
         input_guess: int = int(input('Enter your guess: '))
@@ -55,7 +77,16 @@ def main():
         user_chances -= 1
 
     if is_correct:
-        print(f'Congratulations! You guessed the correct number in {user_chances} attempts.')
+        chances = life - user_chances
+
+        print(f'Congratulations! You guessed the correct number in {chances} attempts.')
+        best_score = highscores.get(difficulty_choice.name)
+
+        if best_score is None or chances < best_score:
+            highscores[difficulty_choice.name] = chances
+            save_highscores(highscores)
+
+            print('New best score!!!')
     else:
         print(f'Sorry.. the correct number is {random_number}')
 
